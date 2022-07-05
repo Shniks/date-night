b5972c99e3ba3b71cbbe3b744b3429aa78a77460
Nikhil Shahi <shniks@gmail.com>
2022-07-05 16:39:46 -0700

    Created method to insert a head node. Added a parent instance variable to Node class.

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -3,10 +3,14 @@ require 'pry'
 
 class BinarySearchTree
 
-  attr_reader :head 
+  attr_reader :head
 
   def initialize
     @head = nil
+  end
+
+  def insert_head_node(score, title, parent = nil)
+    Node.new(score, title, head)
   end
 
 end
--- lib/node.rb
+++ lib/node.rb
@@ -1,13 +1,14 @@
 class Node
 
   attr_reader :score, :title
-  attr_accessor :node_left, :node_right
+  attr_accessor :node_left, :node_right, :parent
 
-  def initialize(score, title)
+  def initialize(score, title, parent = nil)
     @score = score
     @title = title
     @node_left = nil
     @node_right = nil
+    @parent = parent
   end
 
 end
