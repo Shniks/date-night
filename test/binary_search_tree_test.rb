b36d0775d3fcc4511b9536eb70788182db5ad594
Nikhil Shahi <shniks@gmail.com>
2022-07-05 16:45:49 -0700

    Utilized a setup method in the BST Test class.

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -7,21 +7,20 @@ require './lib/binary_search_tree'
 
 class BinarySearchTreeTest < Minitest::Test
 
-  def test_if_it_exists
-    tree = BinarySearchTree.new
+  def setup
+    @tree = BinarySearchTree.new
+  end
 
-    assert_instance_of BinarySearchTree, tree
+  def test_if_it_exists
+    assert_instance_of BinarySearchTree, @tree
   end
 
   def test_if_the_head_node_is_initially_nil
-    tree = BinarySearchTree.new
-
-    assert_nil tree.head
+    assert_nil @tree.head
   end
 
   def test_it_can_insert_a_head_node
-    tree = BinarySearchTree.new
-    result = tree.insert_head_node(61, "Bill & Ted's Excellent Adventure")
+    result = @tree.insert_head_node(61, "Bill & Ted's Excellent Adventure")
 
     assert_instance_of Node, result
     assert_equal 61, result.score
