aa858b18ca7ef26d9a4e6e636d4846d4dee67e2c
Nikhil Shahi <shniks@gmail.com>
2022-07-06 16:59:44 -0700

    Created test to calculate how many nodes have no left or right value.

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -202,4 +202,16 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal 99, @tree.load('./movies.txt')
   end
 
+  def test_how_many_nodes_have_no_left_or_right_value
+    @tree.insert(98, "Animals United")
+    @tree.insert(58, "Armageddon")
+    @tree.insert(36, "Bill & Ted's Bogus Journey")
+    @tree.insert(93, "Bill & Ted's Excellent Adventure")
+    @tree.insert(86, "Charlie's Angels")
+    @tree.insert(38, "Charlie's Country")
+    @tree.insert(69, "Collateral Damage")
+
+    assert_equal 2, @tree.leaves 
+  end
+
 end
